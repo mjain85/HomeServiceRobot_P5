2d731893ae5465b9bc7dc305aef7d7d7a7a6e930
Mahaveer Jain <mahaveer.jain85@gmail.com>
2020-05-24 20:13:24 +0000

    Updated the RVIZ location to src/rvizConfig folder

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -6,7 +6,7 @@ xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch"  &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch"  &
 sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation_HSR.launch" &
+xterm  -e  " rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/viewNavigation.rviz" &
 sleep 5
 #xterm  -e  " rosrun pick_objects pick_objects" &
 #sleep 1
--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -5,8 +5,8 @@ xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch"  &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch"  &
 sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation_HSR.launch" &
-sleep 10
+xterm  -e  " rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/viewNavigation.rviz" &
+sleep 5
 xterm  -e  " rosrun pick_objects pick_objects" &
 sleep 2
 xterm  -e  " rosrun add_markers add_markers"
